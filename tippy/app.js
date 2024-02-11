795dc22cba84f0491b1817f0b6ac0a47c78461ce
Volodymyr_D <dymyr307@gmail.com>
2024-02-11 22:26:20 +0200

    finished with Tippy

--- tippy/app.js
+++ tippy/app.js
@@ -1,16 +1,4 @@
-const moveto = new MoveTo();
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((btn) => moveto.registerTrigger(btn));
-
-const link = document.querySelector('a');
-link.addEventListener('click', handleClick);
-
-function handleClick(e) {
-  e.preventDefault();
-  const target = document.querySelector(this.getAttribute('href'));
-  moveto.move(target, {
-    duration: 3000,
-  });
-}
+tippy(`[data-tippy-content]`, {
+  theme: 'light',
+  animation: 'scale-extreme',
+});
